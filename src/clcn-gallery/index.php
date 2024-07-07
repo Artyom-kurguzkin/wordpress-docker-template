22726c3459af5096654716b3c311214e0c34de8a
Artyom-kurguzkin <artyom.kurguzkin@gmail.com>
2024-07-07 18:34:34 +0930

    wordpress runs

---
+++ src/clcn-gallery/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package Hello_Dolly
+ * @version 1.7.2
+ */
+/*
+Plugin Name: clcn gallery
+Plugin URI: 
+Description: wordpress plugin to dynamically pull photoes with loggede-in user.
+Author: Artyom Kurguzkin
+Version: 1.0.0
+Author URI: http://ma.tt/
+*/
